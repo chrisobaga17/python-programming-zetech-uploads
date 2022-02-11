38046ac00e41168850716d1555428b1d3b50519a
chrisobaga17 <90032608+chrisobaga17@users.noreply.github.com>
2022-02-11 16:01:12 +0300

    Add files via upload

---
+++ login program.py
@@ -0,0 +1,8 @@
+#program to login
+username=str(input("Enter username"))
+pwd=int(input("Enter password"))
+login={"BSCIT-100-200",123}
+if username in login and pwd in login:
+  print("Login successful")
+else:
+  print("Login not successful")
